eca63370cab65b28df2058ef816628ab27521301
pjblecha <44296698+pjblecha@users.noreply.github.com>
2024-05-31 08:42:26 -0500

    #361 update to thunk handlers

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,25 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
+
 function UsersList() {
-    return 'List of Users';
+    const dispatch = useDispatch();
+
+    const { isLoading, data, error } = useSelector((state) => {
+        return state.users;
+    });
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, [dispatch]);
+
+    if(isLoading) return <div>Loading...</div>;
+
+    if(error) return <div>Error fetching data</div>;
+
+    return <div>{data.length}</div>;
+
+    return "List of Users";
 }
 
 export default UsersList;
